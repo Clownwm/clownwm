5e2c28d4fd6f2cbd223d13a22653fadbcf439344
唔明 <oncwnuOMJawpg76ch2sZDqVZxcu8@git.weixin.qq.com>
2020-09-12 11:12:25 +0800

    修改提示词

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-09-12 10:30:14
+ * @LastEditTime: 2020-09-12 11:10:30
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \project\clownwm\src\App.vue
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="努力学习"/>
   </div>
 </template>
 
